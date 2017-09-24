d9a9f4b96d0fe49d3bb8c1a9f821fe9a512f2eb5
vincentsetiawan <vincoolzzz@gmail.com>
2017-09-24 20:31:46 +0700

    Querying Intermediate Table
    Latihan

--- laracms/app/Http/routes.php
+++ laracms/app/Http/routes.php
@@ -185,8 +185,8 @@ Route::get('/user/{id}/role', function($id){
 });
 
 //Accessing the intermediate Table/Pivot
-Route::get('/user/pivot', function(){
-	$user = User::find(1);
+Route::get('/user/{id}/pivot', function($id){
+	$user = User::find($id);
 
 	foreach ($user->roles as $role){
 		echo $role->pivot->created_at;
