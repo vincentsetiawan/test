353bf48ec3446879127c477b105e4904344d8b2b
vincentsetiawan <vincoolzzz@gmail.com>
2017-09-24 23:07:38 +0700

    Polymorphic Relation Many To Many

--- laracms/app/Http/routes.php
+++ laracms/app/Http/routes.php
@@ -226,3 +226,11 @@ Route::get('photo/{id}/post', function($id){
 	$photo = Photo::findOrFail($id);
 	return $photo->imageable;
 });
+
+//Polymorphic many to many
+Route::get('/post/tag', function(){
+	$post = Post::find(1);
+	foreach ($post->tags as $tag) {
+		echo $tag->name;
+	}
+});
