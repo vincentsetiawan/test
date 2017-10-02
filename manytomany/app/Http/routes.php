b3bd009d5efa95469732c7b32e2f1c7f800f4dc3
vincentsetiawan <vincoolzzz@gmail.com>
2017-10-02 15:08:33 +0700

    Deleting Data

--- manytomany/app/Http/routes.php
+++ manytomany/app/Http/routes.php
@@ -47,3 +47,11 @@ Route::get('/update', function(){
 		}
 	}
 });
+
+Route::get('/delete', function(){
+	$user = User::findOrFail(1);
+
+	foreach ($user->roles as $role) {
+		$role->whereId(3)->delete();
+	}
+});
