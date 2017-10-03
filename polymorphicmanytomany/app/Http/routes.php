5e7e9d6a9ebb3d952067eb00936e501eab9fd392
vincentsetiawan <vincoolzzz@gmail.com>
2017-10-04 00:27:44 +0700

    Deleting Data

--- polymorphicmanytomany/app/Http/routes.php
+++ polymorphicmanytomany/app/Http/routes.php
@@ -57,3 +57,11 @@ Route::get('/update', function(){
 
 	$post->tags()->sync([2]);
 });
+
+Route::get('/delete', function(){
+	$post = Post::find(1);
+
+	foreach ($post->tags as $tag) {
+		$tag->whereId(2)->delete();
+	}
+});
