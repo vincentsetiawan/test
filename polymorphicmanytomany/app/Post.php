43cce8c353497057e38f2f3e83d00f813a7a1ae7
vincentsetiawan <vincoolzzz@gmail.com>
2017-10-03 23:48:47 +0700

    Relationship & Mass Assignment

--- polymorphicmanytomany/app/Post.php
+++ polymorphicmanytomany/app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+	protected $fillable = ['name'];
+
+	public function tags(){
+		return $this->morphToMany('App\Tag', 'taggable');
+	}
 }
--- polymorphicmanytomany/app/Video.php
+++ polymorphicmanytomany/app/Video.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     //
+    protected $fillable = ['name'];
+
+	public function tags(){
+		return $this->morphToMany('App\Tag', 'taggable');
+	}
+
 }
