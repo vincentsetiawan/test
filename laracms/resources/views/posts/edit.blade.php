cc76bd017fbaac7dc01a9af75713d4b31dae9d3a
vincentsetiawan <vincoolzzz@gmail.com>
2017-10-04 21:56:15 +0700

    Preparing The Edit View & Setting Up Token

--- laracms/resources/views/posts/edit.blade.php
+++ laracms/resources/views/posts/edit.blade.php
@@ -1,13 +1,13 @@
 @extends('layout.app')
 
 
-
 @section('content')
 
 	<h1>Edit Post</h1>
 
-	<form action="/posts" method="post">
-		<input type="text" name="title /">
+	<form action="/posts/{{$post->id}}" method="post">
+		<input type="hidden" name="_method" value="PUT" />
+		<input type="text" name="title" value="{{$post->title}}" />
 		{{csrf_field()}}
 		<input type="submit" name="submit" />
 	</form>
