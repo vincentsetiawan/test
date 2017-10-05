998ec753c7e383fec69febb65cb245af6306b940
vincentsetiawan <vincoolzzz@gmail.com>
2017-10-05 19:03:33 +0700

    Uploading Files
    Modify Create View For File Input

--- laracms/resources/views/posts/create.blade.php
+++ laracms/resources/views/posts/create.blade.php
@@ -5,11 +5,16 @@
 @section('content')
 
 	<!--<form action="/posts" method="post">-->
-	{!! Form::open(['method' => 'POST', 'action' => 'PostController@store']) !!}
+	{!! Form::open(['method' => 'POST', 'action' => 'PostController@store', 'files' => 'true']) !!}
 		
 		<div class="form-group">
 			{!! Form::label('title', 'Title : ') !!}	
 			{!! Form::text('title', null, ['class' => 'form-control']) !!}
+		</div>
+
+		<div class="form-group">
+			{!! Form::label('Gambar', 'File : ') !!}	
+			{!! Form::file('gambar', ['class' => 'form-control']) !!}
 		</div>
 
 		<div class="form-group">
