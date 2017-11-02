4cbb277ca38858ce075b604cef6530f34fa780a9
vincentsetiawan <vincoolzzz@gmail.com>
2017-11-02 18:37:06 +0700

    Displaying users

--- procms/app/Http/Controllers/AdminUsersController.php
+++ procms/app/Http/Controllers/AdminUsersController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\User;
+
 class AdminUsersController extends Controller
 {
     /**
@@ -16,7 +18,8 @@ class AdminUsersController extends Controller
     public function index()
     {
         //
-        return view('admin.users.index');
+        $users = User::all();
+        return view(('admin.users.index'), compact('users'));
     }
 
     /**
--- procms/resources/views/admin/users/index.blade.php
+++ procms/resources/views/admin/users/index.blade.php
@@ -1,1 +1,38 @@
-<h1>It's working !</h1>
+@extends('layouts.admin')
+
+@section('content')
+
+	<h1>Users</h1>
+
+<table class="table">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>E-Mail</th>
+			<th>Role</th>
+			<th>Active</th>
+			<th>Created</th>
+			<th>Updated</th>
+		</tr>
+	</thead>
+	<tbody>
+	@if($users)
+		@foreach($users as $user)
+
+		<tr>
+			<td>{{$user->id}}</td>
+			<td>{{$user->name}}</td>
+			<td>{{$user->email}}</td>
+			<td>{{$user->role->name}}</td>
+			<td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
+			<td>{{$user->created_at->diffForHumans()}}</td>
+			<td>{{$user->updated_at->diffForHumans()}}</td>
+		</tr>
+
+		@endforeach
+	@endif
+	</tbody>
+</table>
+
+@stop
