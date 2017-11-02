9bb6b50832f8663ec5e6fd0c4ac23709ec758da9
vincentsetiawan <vincoolzzz@gmail.com>
2017-11-02 19:21:16 +0700

    Create Page

---
+++ procms/resources/views/admin/users/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<h1>Create User</h1>
+
+{!! Form::open(['method' => 'POST', 'action'=>'AdminUsersController@store']) !!}
+
+<div class="form-group">
+	{!! Form::label('name', 'Name: ') !!}
+	{!! Form::text('name', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('email', 'Email: ') !!}
+	{!! Form::email('email', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('role_id', 'Role: ') !!}
+	{!! Form::text('role_id', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('status', 'Status: ') !!}
+	{!! Form::text('status', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+</div>
+
+{!! Form::close() !!}
+
+@stop
