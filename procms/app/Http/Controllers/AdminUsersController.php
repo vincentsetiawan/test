72aa227ac54b1ba295ca0fb6b8252b207e739927
vincentsetiawan <vincoolzzz@gmail.com>
2017-11-16 19:05:50 +0700

    Persisting Data & Fixing Bug

--- procms/app/Http/Controllers/AdminUsersController.php
+++ procms/app/Http/Controllers/AdminUsersController.php
@@ -45,7 +45,8 @@ class AdminUsersController extends Controller
     public function store(UsersRequest $request)
     {
         //
-        return $request->all();
+        User::create($request->all());
+        return redirect('/admin/users');
     }
 
     /**
